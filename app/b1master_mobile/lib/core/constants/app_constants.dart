2e20cbfde4d14397e3ea0f5ac9265d1897559ca6
B1MasterAI <sanjay.mail.info@gmail.com>
2026-07-31 23:03:12 +0530

    Added core architecture files

---
+++ app/b1master_mobile/lib/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String appName = "B1Master Academy";
+  static const String appTagline = "SAP Business One Learning Platform";
+
+  static const String version = "1.0.0";
+
+  static const String developer = "B1MasterAI";
+}
---
+++ app/b1master_mobile/lib/core/routes/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const splash = "/";
+  static const login = "/login";
+  static const home = "/home";
+  static const videos = "/videos";
+  static const commands = "/commands";
+  static const interview = "/interview";
+  static const profile = "/profile";
+}
